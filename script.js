c1cb9027e4d2b7b526c75a5fca98179f9fafe199
wakana <wakanawardrobe@gmail.com>
2022-10-12 16:33:32 +0100

    sorting is working

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-let array = [5,2,9,7];
+const merge = (left, right) => {
 
-const splitArray = (array) => {
+    const merged = [...left, ...right];
+    const sorted = merged.sort((a, b) => a - b);
+
+    return sorted;
+}
+
+const mergeSort = (array) => {
+
+    if (array.length === 1) {
+        return array;
+    }
+
     const half = Math.round(array.length / 2);
 
-    let split1 = [];
-    let split2 = [];
+    let left = [];
+    let right = [];
 
     for (let i=0; i<half; i++) {
-        split1.push(array[i]);
+        left.push(array[i]);
     }
 
     for (let i=half; i<array.length; i++) {
-        split2.push(array[i]);
+        right.push(array[i]);
     }
-    console.log(split1);
-    console.log(split2);
 
-
+    return merge(mergeSort(left), mergeSort(right));
 
 }
 
-splitArray(array);
+// console.log(mergeSort([1,6,3,4,5,2,7,8]));
+console.log(mergeSort([567,2,8,1,98,67,500]));
+
+//function takes in an array of numbers, divide the array by 2 so its a half. Which will give us two new arrays. Keep dividing the array by recursively calling the original function, until we have an array with a length of 1.
